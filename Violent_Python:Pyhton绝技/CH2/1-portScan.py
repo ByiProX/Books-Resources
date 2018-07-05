1ccc8e6c2bde2175b2adca1a3634000d2ea00e28
Quentin Wang <wangkx0105@outlook.com>
2018-07-05 15:55:51 +0800

    modify python2 to 3

--- Violent_Python:Pyhton绝技/CH2/1-portScan.py
+++ Violent_Python:Pyhton绝技/CH2/1-portScan.py
@@ -2,60 +2,64 @@
 # -*- coding: utf-8 -*-
 
 import optparse
-from socket import *
-from threading import *
+import socket
+import threading
 
-screenLock = Semaphore(value=1)
+screenLock = threading.Semaphore(value=1)
+
 
 def connScan(tgtHost, tgtPort):
+    connSkt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
         connSkt.send('ViolentPython\r\n')
         results = connSkt.recv(100)
         screenLock.acquire()
-        print '[+] %d/tcp open' % tgtPort
-        print '[+] ' + str(results)
-    except:
+        print('[+] %d/tcp open' % tgtPort)
+        print('[+] ' + str(results))
+    except Exception:
         screenLock.acquire()
-        print '[-] %d/tcp closed' % tgtPort
+        print('[-] %d/tcp closed' % tgtPort)
     finally:
-	screenLock.release()
-	connSkt.close()	
+        screenLock.release()
+        connSkt.close()
+
 
 def portScan(tgtHost, tgtPorts):
     try:
-        tgtIP = gethostbyname(tgtHost)
-    except:
-        print "[-] Cannot resolve '%s': Unknown host" %tgtHost
+        tgtIP = socket.gethostbyname(tgtHost)
+    except Exception:
+        print("[-] Cannot resolve '%s': Unknown host" % tgtHost)
         return
 
     try:
-        tgtName = gethostbyaddr(tgtIP)
-        print '\n[+] Scan Results for: ' + tgtName[0]
-    except:
-        print '\n[+] Scan Results for: ' + tgtIP
+        tgtName = socket.gethostbyaddr(tgtIP)
+        print('\n[+] Scan Results for: ' + tgtName[0])
+    except Exception:
+        print('\n[+] Scan Results for: ' + tgtIP)
 
-    setdefaulttimeout(1)
+    socket.setdefaulttimeout(1)
+    # tgtPorts = range(10000)
     for tgtPort in tgtPorts:
-        t = Thread(target=connScan,args=(tgtHost,int(tgtPort)))
+        t = threading.Thread(target=connScan, args=(tgtHost, int(tgtPort)))
         t.start()
 
-def main():
-    parser = optparse.OptionParser('usage %prog '+\
-      '-H <target host> -p <target port>')
-    parser.add_option('-H', dest='tgtHost', type='string',\
-      help='specify target host')
-    parser.add_option('-p', dest='tgtPort', type='string',\
-      help='specify target port[s] separated by comma')
 
-    (options, args) = parser.parse_args()
+def main():
+    parser = optparse.OptionParser('usage %prog ' +
+                                   '-H <target host> -p <target port>')
+    parser.add_option('-H', dest='tgtHost', type='string',
+                      help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string',
+                      help='specify target port[s] separated by comma')
+
+    options, args = parser.parse_args()
 
     tgtHost = options.tgtHost
     tgtPorts = str(options.tgtPort).split(',')
 
-    if (tgtHost == None) | (tgtPorts[0] == None):
-	print parser.usage
+    if (tgtHost is None) or (tgtPorts[0] is None):
+        print(parser.usage)
         exit(0)
 
     portScan(tgtHost, tgtPorts)
--- Violent_Python:Pyhton绝技/CH2/2-nmapScan.py
+++ Violent_Python:Pyhton绝技/CH2/2-nmapScan.py
@@ -1,27 +1,29 @@
 import nmap
 import optparse
 
-def nmapScan(tgtHost,tgtPort):
+
+def nmapScan(tgtHost, tgtPort):
     nmScan = nmap.PortScanner()
-    nmScan.scan(tgtHost,tgtPort)
-    state=nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
-    print "[*] " + tgtHost + " tcp/"+tgtPort +" "+state
+    nmScan.scan(tgtHost, tgtPort)
+    state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+    print("[*] " + tgtHost + " tcp/" + tgtPort + " " + state)
+
 
 def main():
-    parser = optparse.OptionParser('usage %prog '+\
+    parser = optparse.OptionParser('usage %prog ' + \
                                    '-H <target host> -p <target port>')
-    parser.add_option('-H', dest='tgtHost', type='string',\
+    parser.add_option('-H', dest='tgtHost', type='string', \
                       help='specify target host')
-    parser.add_option('-p', dest='tgtPort', type='string',\
+    parser.add_option('-p', dest='tgtPort', type='string', \
                       help='specify target port[s] separated by comma')
-    
+
     (options, args) = parser.parse_args()
-    
+
     tgtHost = options.tgtHost
     tgtPorts = str(options.tgtPort).split(',')
-    
-    if (tgtHost == None) | (tgtPorts[0] == None):
-        print parser.usage
+
+    if (tgtHost is None) | (tgtPorts[0] is None):
+        print(parser.usage)
         exit(0)
     for tgtPort in tgtPorts:
         nmapScan(tgtHost, tgtPort)
@@ -29,4 +31,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
